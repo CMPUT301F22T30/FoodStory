b72eb46fbb982291498de415ff73fca5a1e2d416
rehmatul <114538780+rehmatul@users.noreply.github.com>
2022-10-29 20:31:46 -0600

    Display Recipe complete

--- code/app/src/main/java/com/example/foodstory/RecipeClass.java
+++ code/app/src/main/java/com/example/foodstory/RecipeClass.java
@@ -11,13 +11,29 @@ public class RecipeClass {
     private String photo;
     private ArrayList<Ingredient> ingredients;
 
-    public RecipeClass(String title, String prepTime, int numServings, String recipeCategory, String comments, String photo, ArrayList<Ingredient> ingredients) {
+    public RecipeClass(String title, String prepTime, int numServings, String recipeCategory, String comments, String photo) {
         this.title = title;
         this.prepTime = prepTime;
         this.numServings = numServings;
         this.recipeCategory = recipeCategory;
         this.comments = comments;
         this.photo = photo;
-        this.ingredients = ingredients;
+        //this.ingredients = ingredients;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRecipeCategory() {
+        return recipeCategory;
+    }
+
+    public void setRecipeCategory(String recipeCategory) {
+        this.recipeCategory = recipeCategory;
     }
 }
--- code/app/src/main/java/com/example/foodstory/RecipeFragment.java
+++ code/app/src/main/java/com/example/foodstory/RecipeFragment.java
@@ -1,10 +1,51 @@
 package com.example.foodstory;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-public class RecipeFragment extends Fragment {
-    public RecipeFragment() {
+import com.example.foodstory.databinding.RecipeFragmentBinding;
 
-        super(R.layout.home_fragment);
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipeFragment extends Fragment {
+    private RecipeFragmentBinding binding;
+
+    public RecipeFragment(){
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = RecipeFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ArrayList<RecipeClass> recipe_List = new ArrayList<RecipeClass>();
+        ArrayAdapter<RecipeClass> recipe_Adapter = new RecipeAdapter(getActivity(),recipe_List);
+        ListView recipeList = getView().findViewById(R.id.recipe_list);
+        recipeList.setAdapter(recipe_Adapter);
+        RecipeClass testRecipe = new RecipeClass("abcd", "cd", 12, "efg", "de", "xyz");
+        recipe_List.add(testRecipe);
+        recipe_Adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
