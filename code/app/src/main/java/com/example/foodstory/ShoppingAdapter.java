b4e24b564466343cc7340bb0f83ff6e36352a172
kashishm007 <50669884+kashishm007@users.noreply.github.com>
2022-11-27 01:41:16 -0700

    Shopping List is created, which shows the ingredients and its details

---
+++ code/app/src/main/java/com/example/foodstory/ShoppingAdapter.java
@@ -0,0 +1,44 @@
+package com.example.foodstory;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class ShoppingAdapter extends ArrayAdapter<Ingredient> {
+    private ArrayList<Ingredient> ingredients;
+    private Context context;
+
+    public ShoppingAdapter(Context context, ArrayList<Ingredient> ingredients) {
+        super(context, 0, ingredients);
+        this.ingredients = ingredients;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        return super.getView(position, convertView, parent);
+        View view = convertView;
+        view = LayoutInflater.from(context).inflate(R.layout.ingredient_in_shoppinglist_display, parent,false);
+        Ingredient ingredient = ingredients.get(position);
+        TextView ingredientTitle= view.findViewById(R.id.ingredient_title);
+        TextView ingredientdesc = view.findViewById(R.id.ingredient_description);
+        TextView ingredientamount= view.findViewById(R.id.ingredient_amount);
+        TextView ingredientCategory = view.findViewById(R.id.ingredient_category);
+        TextView ingredientUnit = view.findViewById(R.id.ingredient_unit);
+        ingredientTitle.setText(ingredient.getName());
+        ingredientamount.setText(String.valueOf(ingredient.getAmount()));
+        ingredientdesc.setText(ingredient.getDescription());
+        ingredientCategory.setText(ingredient.getCategory());
+        ingredientUnit.setText(String.valueOf(ingredient.getUnit()));
+        return view;
+    }
+}
---
+++ code/app/src/main/java/com/example/foodstory/ShoppingListFragment.java
@@ -0,0 +1,120 @@
+package com.example.foodstory;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.foodstory.databinding.IngredientFragmentBinding;
+import com.example.foodstory.databinding.ShoppingListFragmentBinding;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ShoppingListFragment extends Fragment {
+    private ShoppingListFragmentBinding binding;
+    FirebaseFirestore dbShopDisp;
+    Ingredient shopping_ingredient;
+    Ingredient ingredient;
+
+    public ShoppingListFragment(){
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = ShoppingListFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ArrayList<Ingredient> shopping_ingredients_List = new ArrayList<Ingredient>();
+        ArrayList<Ingredient> ingredients_List = new ArrayList<Ingredient>();
+        ArrayAdapter<Ingredient> shopping_ingredient_Adapter = new ShoppingAdapter(getActivity(),shopping_ingredients_List);
+        ListView shoppingIngredientList = getView().findViewById(R.id.shopping_ingredient_list);
+        shoppingIngredientList.setAdapter(shopping_ingredient_Adapter);
+        dbShopDisp = FirebaseFirestore.getInstance();
+        CollectionReference ShoppingListReference = dbShopDisp.collection("ShoppingList");
+        CollectionReference IngredientReference = dbShopDisp.collection("Ingredients");
+
+//        binding.shoppingIngredientList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Ingredient passedIngredient = ingredients_List.get(i);
+//
+//                Bundle caller = new Bundle();
+//                //bundle key = "recipe"
+//                //recipe.putSerializable("recipe", passedRecipe);
+//                //request key = "recipeKey"
+//                // To communicate with the AddIngredientFragment that AddRecipeFragment is the caller.
+//                caller.putString("parentFragment", "DisplayShoppingListFragment");
+//                caller.putSerializable("IngredientObj", passedIngredient);
+//                caller.putString("ingredientName", passedIngredient.getName());
+//                getParentFragmentManager().setFragmentResult("callerKey", caller);
+//                NavHostFragment.findNavController(ShoppingListFragment.this)
+//                        .navigate(R.id.action_ShoppingListFragment_to_DisplayIngredientFragment);
+//            }
+//        });
+
+        binding.ShoppingListtoHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ShoppingListFragment.this)
+                        .navigate(R.id.action_ShoppingListFragment_to_HomeFragment);
+            }
+        });
+        IngredientReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                ingredients_List.clear();
+                for(QueryDocumentSnapshot doc: queryDocumentSnapshots)
+                {
+                    ingredient = doc.toObject(Ingredient.class);
+                    ingredients_List.add(ingredient);
+                }
+
+            }
+        });
+
+        ShoppingListReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable
+                    FirebaseFirestoreException error) {
+                shopping_ingredients_List.clear();
+                for(QueryDocumentSnapshot doc: queryDocumentSnapshots)
+                {
+                    shopping_ingredient = doc.toObject(Ingredient.class);
+//                    if (ingredients_List.contains(shopping_ingredient)){
+//                        int index = ingredients_List.indexOf(shopping_ingredient);
+//                        int ingredient_amount = ingredients_List.get(index).getAmount();
+//                        shopping_ingredient.setAmount(ingredient_amount);
+//                    }
+                    shopping_ingredient.setAmount(6);
+                    shopping_ingredients_List.add(shopping_ingredient);
+
+                }
+                shopping_ingredient_Adapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched from the cloud
+            }
+        });
+    }
+}
